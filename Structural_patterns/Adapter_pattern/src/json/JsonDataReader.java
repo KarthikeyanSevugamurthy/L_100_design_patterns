99593e8589d8ae551fa971c0e5d89f8daab6d650
Karthikeyan <karthikeyan.s@ei.study>
2025-06-24 22:36:23 +0530

    Revert "feat: add adapter , composite and Proxy pattern"
    This reverts commit dcf54c4a4dda8ed8547f69177073b7ecdac6437b.

--- Structural_patterns/Adapter_pattern/src/json/JsonDataReader.java
+++
@@ -1,5 +0,0 @@
-package json;
-
-public interface JsonDataReader {
-    String getJsonData();
-}
--- Structural_patterns/Composite_pattern/src/component/EmployeeComponent.java
+++
@@ -1,5 +0,0 @@
-package component;
-
-public interface EmployeeComponent {
-    void showDetails();
-}
